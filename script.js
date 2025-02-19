8ee1ba6b66acf2a90d69755b930b5207b64591fd
wangaribot <wangaricanadakelvin@gmail.com>
2025-02-19 16:28:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close mobile menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Swiper slider configuration
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+  grabCursor: true,
+  SpaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //REsponsive breakpoints
+
+  breakpoint: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
